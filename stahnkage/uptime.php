08966b94c4d77578c45c3bb6b287d69360142afb
Michael Stahnke <stahnma@duo.stahnkage.com>
2008-11-21 20:57:30 -0600

    Fixes on uptime.php

--- stahnkage/uptime.php
+++ stahnkage/uptime.php
@@ -1,18 +1,12 @@
 <?php
 /**
- * Sample File 2, phpDocumentor Quickstart
+ * Uptime 
  *
- * This file demonstrates the rich information that can be included in
- * in-code documentation through DocBlocks and tags.
- * @author Greg Beaver <cellog@php.net>
+ * This file computes your uptime for the system.  
+ * @author Michael Stahnke <stahnma@fedoraproject.org>
  * @version 1.0
  * @package sample
  */
-// sample file #1
-/**
- * Dummy include value, to demonstrate the parsing power of phpDocumentor
- */
-
 
  $a=exec("uptime");
  $arr = explode(' ', $a);
