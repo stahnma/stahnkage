5b759d0853b9887042397633878a62e4d6d23bbd
Michael Stahnke <stahnma@duo.stahnkage.com>
2008-11-21 21:04:29 -0600

    Removing junk

--- stahnkage/footer_text.php
+++
@@ -1,2 +0,0 @@
-Copyright &copy; <?php print date('Y'); ?>
- by the guys who bring you Stahnkage. <br /> Remember the world is better with Open Source software.  If you see source you want here, use it.
